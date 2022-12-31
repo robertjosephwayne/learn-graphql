8c0dcb8774cbe70d8f6bfe28f7d3c32598c0d220
Robert Joseph Wayne <robertjwayne@gmail.com>
2022-12-31 18:28:52 +0700

    Wrap app in ApolloProvider

--- tutorials/frontend/react-apollo-hooks/app-boilerplate/src/components/App.js
+++ tutorials/frontend/react-apollo-hooks/app-boilerplate/src/components/App.js
@@ -38,24 +38,26 @@ const App = () => {
     return <Login />;
   }
   return (
-    <div>
-      <Header logoutHandler={logout} />
-      <div className="row container-fluid p-left-right-0 m-left-right-0">
-        <div className="row col-md-9 p-left-right-0 m-left-right-0">
-          <div className="col-md-6 sliderMenu p-30">
-            <TodoPrivateWrapper />
+    <ApolloProvider client={client}>
+      <div>
+        <Header logoutHandler={logout} />
+        <div className="row container-fluid p-left-right-0 m-left-right-0">
+          <div className="row col-md-9 p-left-right-0 m-left-right-0">
+            <div className="col-md-6 sliderMenu p-30">
+              <TodoPrivateWrapper />
+            </div>
+            <div className="col-md-6 sliderMenu p-30 bg-gray border-right">
+              <TodoPublicWrapper />
+            </div>
           </div>
-          <div className="col-md-6 sliderMenu p-30 bg-gray border-right">
-            <TodoPublicWrapper />
-          </div>
-        </div>
-        <div className="col-md-3 p-left-right-0">
-          <div className="col-md-12 sliderMenu p-30 bg-gray">
-            <OnlineUsersWrapper />
+          <div className="col-md-3 p-left-right-0">
+            <div className="col-md-12 sliderMenu p-30 bg-gray">
+              <OnlineUsersWrapper />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </ApolloProvider>
   );
 };
 
